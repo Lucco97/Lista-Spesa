8df0c22821ee75220094c6ee91a6de8aac00c8cc
Andrea <spada96@hotmail.it>
2015-02-21 12:52:00 +0100

    Update ViewController.swift

--- Lista Spesa/ViewController.swift
+++ Lista Spesa/ViewController.swift
@@ -1,10 +1,10 @@
 //
 //  ViewController.swift
 //  Lista Spesa
-//
-//  Created by Fabio Luccon on 24/01/15.
+//  
+//  Created by Fabio Luccon and Andrea Spada on 24/01/15.
 //  Copyright (c) 2015 Kennedy. All rights reserved.
-//
+// 
 
 import UIKit
 
